43bc814a7af094e5a6c9e27727f342661384b4be
Milton <117595866+MiltonSandoval@users.noreply.github.com>
2024-01-25 20:51:40 -0400

    Add files via upload

---
+++ cambiar_vocales.cpp.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+//Variables globales.
+string palabras;
+
+void cambiarVocales(char vocal, char numero)
+{	int site = 0;
+	do
+	{
+		site = palabras.find(vocal,site);
+		if (site != -1)
+			palabras[site]=numero;
+		site++;
+	} while(site !=0);
+}
+
+int main(int argc, char *argv[]) {
+	palabras = "Hola mundo, aeiou";
+	cambiarVocales('a','4');
+	cambiarVocales('e','3');
+	cambiarVocales('i','1');
+	cambiarVocales('o','0');
+	cambiarVocales('u','5');
+	cout<<"Palabra cambiada:"<<palabras;
+	return 0;
+}
+
