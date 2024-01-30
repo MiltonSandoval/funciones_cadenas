df2c729f663e583ac3049c668be3953a82d7b5ed
Milton <117595866+MiltonSandoval@users.noreply.github.com>
2024-01-29 21:48:40 -0400

    Add files via upload

---
+++ menu_frutas.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+string frutas[] = {"Manzana", "Banana", "Naranja", "Fresa", "Kiwi", "Mango", "Pera", "Uva", "Melón", "Papaya"};
+string preparados[] = {"Jugo", "Ensalada", "Frapé", "Tarta", "Helado", "Mermelada", "Batido", "Asado", "Cóctel", "Crudo"};
+string fruta[5];
+string preparado[5];
+
+string generarFruta()
+{
+	return frutas[rand()%10];
+}
+string generarPreparado()
+{
+	return preparados[rand()%10];
+}
+void generar(){
+	for(int i=0;i<5;i++)
+	{
+		fruta[i]=generarFruta();
+		preparado[i]=generarPreparado();
+	}
+}	
+	void imprimir_datos()
+	{
+		cout<<"Menu de productos"<<endl;
+		for(int i=0;i<5;i++)
+			cout<<preparado[i]<<" de "<<fruta[i]<<endl;
+	}
+	
+	int main(int argc, char *argv[]) {
+		generar();
+		imprimir_datos();
+		return 0;
+	}
+	
